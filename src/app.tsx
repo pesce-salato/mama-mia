4c030b3380f42769af9d368ec2ea6d2ca34ee521
mayfly <59794462+x-glorious@users.noreply.github.com>
2023-04-17 17:37:59 +0800

    chore: create project with react、typescript、webpack、prettier、eslint

---
+++ src/app.tsx
@@ -0,0 +1,8 @@
+import * as ReactDOM from 'react-dom';
+import {Preprocess} from "./views/preprocess";
+
+const App = () => {
+  return <Preprocess />
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
---
+++ src/views/preprocess/index.tsx
@@ -0,0 +1,3 @@
+export const Preprocess = () => {
+  return <div>Yes</div>
+}
