84f8b69add3234f29a9df493595d893d8c8b0567
mayfly <59794462+x-glorious@users.noreply.github.com>
2023-05-06 17:48:50 +0800

    feat: add db service

---
+++ src/services/db/config.ts
@@ -0,0 +1,6 @@
+import Path from 'path'
+import { io } from '@/services/io'
+
+export const config = {
+  credentials: Path.join(io.config.path.db, 'credentials.json'),
+}
---
+++ src/services/db/index.ts
@@ -0,0 +1,5 @@
+import { config } from './config'
+
+export const db = {
+  config,
+}
