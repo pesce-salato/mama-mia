6868ac89097b809ecc865d00be7995bae091799b
mayfly <59794462+x-glorious@users.noreply.github.com>
2023-05-11 21:30:45 +0800

    feat: add service

--- src/services/main.ts
+++ src/services/main.ts
@@ -3,12 +3,16 @@ import { ExecutorService } from '@/services/executor/main'
 import { WindowService } from '@/services/window/main'
 import { CredentialService } from '@/services/credential/main'
 import { IoService } from '@/services/io/main'
+import { PluginServices } from '@/services/plugin/main'
+import { ToolService } from '@/services/tool/main'
 
 export interface Services {
   executor: ExecutorService
   window: WindowService
   credential: CredentialService
   io: IoService
+  plugin: PluginServices
+  tool: ToolService
 }
 
 export const generateServices = (context: MainProcessContext): Services => {
@@ -17,5 +21,7 @@ export const generateServices = (context: MainProcessContext): Services => {
     window: new WindowService(context),
     credential: new CredentialService(context),
     io: new IoService(),
+    plugin: new PluginServices(),
+    tool: new ToolService(context),
   }
 }
--- src/services/renderer.ts
+++ src/services/renderer.ts
@@ -1,11 +1,13 @@
 import { ExecutorService } from '@/services/executor/renderer'
 import { WindowService } from '@/services/window/renderer'
 import { CredentialService } from '@/services/credential/renderer'
+import { ToolService } from '@/services/tool/renderer'
 
 export interface Services {
   executor: ExecutorService
   window: WindowService
   credential: CredentialService
+  tool: ToolService
 }
 
 export const generateServices = (): Services => {
@@ -13,5 +15,6 @@ export const generateServices = (): Services => {
     executor: new ExecutorService(),
     window: new WindowService(),
     credential: new CredentialService(),
+    tool: new ToolService(),
   }
 }
